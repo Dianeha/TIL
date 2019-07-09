aa47ab5c5b12ba77663d8488fabd498824689bed
Dayeon Ha <gvbhjnmk@gamil.com>
2019-07-09 14:43:16 +0900

    startcamp | finish webbrowser | 190709

---
+++ 00_startcamp/day02/web_browser.py
@@ -0,0 +1,13 @@
+import webbrowser
+
+urls = [
+    'http://edu.ssafy.com',
+    'www.google.com',
+    'https://github.com/Dianeha',
+    'https://2-ss3.slack.com/messages/CKWED8XAN/team/UL7PVL2VA/',
+    'www.youtube.com'
+]
+
+for url in urls:
+    webbrowser.open(url)
+
