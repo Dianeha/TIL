e584d53b7064d770b1d0d04aad647270cad08d30
Dianeha <gvbhjnmk@gmail.com>
2019-11-06 18:16:48 +0900

    06_JS | Vue.js basic grammar | 191106

---
+++ 07_vue/04_ServerClient/first-vue-cli/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+<!-- 반드시, Template 안에는 "단 한개"의 최상단 태그(노드)가 존재해야함. 여기선 div -->
+    <div>
+        <h1>나쁜말 거름종이</h1>
+        <input type="text" v-model="input">
+        <p>{{ filteredInput }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+    // 왜인지는 모르겠지만, 이제부터는 data: function(){return{}}으로 만들어야해
+    data: function () {
+        return {
+            input: 'Type something'
+        }
+    },
+    methods: {},
+    computed: {
+        filteredInput: function () {
+            // [바보] 가 들어오면, ** 로 바꿔서 return 하도록 한다.
+            let result = this.input.replace(/바보|멍청이|똥개/gi, '##');
+            return `Filtered: ${result}`;
+        },
+    }
+}
+</script>
+
+<style></style>
---
+++ 07_vue/04_ServerClient/first-vue-cli/src/main.js
@@ -0,0 +1,8 @@
+import Vue from 'vue';
+import App from './App.vue'; // .vue 는 안써도 알아서 동작함
+
+new Vue({
+    // el: '#app',
+    // method(함수 in 객체) 정의할 때, () => {} 금지이지만, 여기서만 쓴다.
+    render: h => h(App),
+}).$mount('#app') // .$mount('#app') === el: '#app' 같은 역할. 주로 전자를 쓴다.
